f770943cc7b1088fcbc8bfbdde5b30d6a8c9e470
TPGoFighting <17356397030@163.com>
2026-02-05 23:26:45 +0800

    准备部署到Render

--- My_Blog/venv/Scripts/activate.bat
+++ My_Blog/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=E:\ASpecializedRoomForLearing\My_Blog\venv
+set VIRTUAL_ENV=E:\ASpecializedRoomForLearing\Personal_Project\My_Blog\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
