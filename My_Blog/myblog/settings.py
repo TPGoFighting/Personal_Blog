23e56dbdd3472ebceb231ada86d414802eb69a0a
TPGoFighting <17356397030@163.com>
2026-02-06 00:09:26 +0800

    新增微信打开支持

--- My_Blog/myblog/settings.py
+++ My_Blog/myblog/settings.py
@@ -27,6 +27,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+import dj_database_url
+DATABASES = {
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3', 
+        conn_max_age=600
+    )
+}
 
 # Application definition
 
